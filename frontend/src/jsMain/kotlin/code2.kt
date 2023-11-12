f7c55d9c33b3aa17d9ad65bd7f087b5ece474787
Vladimir Genovich <genovich@fb.com>
2023-11-12 15:27:57 +0000

    taggable primitives click-on-a-part behaviour

--- frontend/src/jsMain/kotlin/code2.kt
+++ frontend/src/jsMain/kotlin/code2.kt
@@ -4,19 +4,59 @@ import org.w3c.dom.CanvasRenderingContext2D
 import org.w3c.dom.HTMLCanvasElement
 import org.w3c.dom.events.MouseEvent
 
-class GraphicalLayer<Key : Hashable>(
+class GraphicalLayer<KeyType : Hashable>(
   val canvas: HTMLCanvasElement,
-  val onClick: GraphicalLayer<Key>.(ClickEvent<Key>) -> Unit
+  val onClick: GraphicalLayer<KeyType>.(ClickEvent<KeyType>) -> Unit
 ) {
 
-  sealed interface ClickEvent<Key : Hashable>
-  data class EmptySpaceClicked<Key : Hashable>(val x: Int, val y: Int) : ClickEvent<Key>
-  data class PrimitiveClicked<Key : Hashable>(val tag: Hashable) : ClickEvent<Key>
+  sealed interface ClickEvent<KeyType : Hashable>
+  data class EmptySpaceClicked<KeyType : Hashable>(val x: Int, val y: Int) : ClickEvent<KeyType>
+  data class PrimitiveClicked<KeyType : Hashable>(val path: List<KeyType>, val x: Int, val y: Int) :
+    ClickEvent<KeyType>
 
   private val drawingContext = canvas.getContext("2d") as CanvasRenderingContext2D
-  private val primitives = mutableListOf<Primitive>()
-  private val tagToPrimitive = mutableMapOf<Hashable, Primitive>()
-  private val primitiveToTag = mutableMapOf<Primitive, Hashable>()
+
+  sealed interface KeyOrRoot<out KeyType : Hashable> : Hashable
+  data object RootKey : KeyOrRoot<Nothing>
+  data class Key<KeyType : Hashable>(val value: KeyType) : KeyOrRoot<KeyType>
+
+  private val root = Stack(0, 0, canvas.height, canvas.width)
+  private val hierarchy = mutableMapOf<Stack, MutableList<Primitive>>(root to mutableListOf())
+  private val primitivesKeys = mutableMapOf<Primitive, KeyOrRoot<KeyType>>(root to RootKey)
+
+  private fun Stack.add(primitive: Primitive, key: KeyType?) {
+    hierarchy.getOrPut(this) { mutableListOf() }.add(primitive)
+    if (key != null) primitivesKeys[primitive] = Key(key)
+  }
+
+  private fun Stack.recalculateSize() {
+    hierarchy[this]?.also { children ->
+      width = children.maxOf { it.x + it.width } - children.minOf { it.x }
+      height = children.maxOf { it.y + it.height } - children.minOf { it.y }
+    }
+  }
+
+  private fun Primitive.findAt(targetX: Int, targetY: Int): List<Primitive> {
+    return when (this) {
+      is Rectangle -> findAt(targetX, targetY)
+      is Stack -> findAt(targetX, targetY)
+    }
+  }
+
+  private fun Rectangle.findAt(targetX: Int, targetY: Int): List<Primitive> {
+    return listOfNotNull(takeIf { targetX - x in 0..width && targetY - y in 0..height })
+  }
+
+  private fun Stack.findAt(targetX: Int, targetY: Int): List<Primitive> {
+    val localX = targetX - x
+    val localY = targetY - y
+    return if (localX in 0..width && localY in 0..height) {
+      listOf(this) + hierarchy.getOrPut(this) { mutableListOf() }
+        .flatMap { it.findAt(localX, localY) }
+    } else {
+      emptyList()
+    }
+  }
 
   init {
     drawingContext.fillStyle = "rgb(30 31 34)"
@@ -26,8 +66,10 @@ class GraphicalLayer<Key : Hashable>(
     canvas.addEventListener("mousedown", { event ->
       event as MouseEvent
 
-      movingContext = primitives
-        .findLast { it.x <= event.clientX && event.clientX <= it.x + it.width && it.y <= event.clientY && event.clientY <= it.y + it.height }
+      movingContext = root.findAt(event.clientX, event.clientY)
+        .filterIsInstance<Stack>()
+        .minus(root) // root is not draggable, yet
+        .lastOrNull()
         ?.let { MovingContext(it, event.clientX - it.x, event.clientY - it.y) }
     })
     canvas.addEventListener("mousemove", { event ->
@@ -41,12 +83,9 @@ class GraphicalLayer<Key : Hashable>(
       if (movingContext?.dirty != true) {
         event as MouseEvent
 
-        val found = primitives
-          .findLast { it.x <= event.clientX && event.clientX <= it.x + it.width && it.y <= event.clientY && event.clientY <= it.y + it.height }
-          ?.let { primitiveToTag[it] }
-
-        if (found != null) {
-          onClick(PrimitiveClicked(found))
+        val keys = root.findAt(event.clientX, event.clientY).mapNotNull { (primitivesKeys[it] as? Key<KeyType>)?.value }
+        if (keys.isNotEmpty()) {
+          onClick(PrimitiveClicked(keys, event.clientX, event.clientY))
         } else {
           onClick(EmptySpaceClicked(event.clientX, event.clientY))
         }
@@ -55,7 +94,7 @@ class GraphicalLayer<Key : Hashable>(
     })
   }
 
-  inner class MovingContext(val primitive: Primitive, val offsetX: Int, val offsetY: Int) {
+  private inner class MovingContext(val primitive: Primitive, val offsetX: Int, val offsetY: Int) {
 
     var dirty: Boolean = false
       private set
@@ -67,24 +106,45 @@ class GraphicalLayer<Key : Hashable>(
     }
   }
 
-  sealed interface Primitive
+  sealed interface Primitive : Hashable
 
-  data class Rectangle(var x: Int = 0, var y: Int = 0, val width: Int = 100,
-    val height: Int = 100) :
-    Primitive
+  data class Rectangle(var x: Int, var y: Int, val width: Int, val height: Int) : Primitive {
 
-  data class Stack(var x: Int, var y: Int, val primitives: List<Primitive>) : Primitive {
-
-    val width: Int = primitives.maxOf { it.x + it.width } - primitives.minOf { it.x }
-    val height: Int = primitives.maxOf { it.y + it.height } - primitives.minOf { it.y }
+    // todo find another way to use primitives as keys
+    private val key = intArrayOf(x, y, width, height)
+    override fun equals(other: Any?) = (other as? Rectangle)?.key.contentEquals(key)
+    override fun hashCode() = key.hashCode()
   }
 
-  fun EmptySpaceClicked<*>.createStackHereFor(tag: Key, item: Primitive, vararg items: Primitive) {
-    Stack(x, y, listOf(item) + items).also {
-      primitives.add(it)
-      tagToPrimitive[tag] = it
-      primitiveToTag[it] = tag
+  data class Stack(var x: Int, var y: Int, var width: Int, var height: Int) : Primitive {
+
+    // todo find another way to use primitives as keys
+    private val key = intArrayOf(x, y, width, height)
+    override fun equals(other: Any?) = (other as? Stack)?.key.contentEquals(key)
+    override fun hashCode() = key.hashCode()
+  }
+
+  @DslMarker
+  annotation class StackBuilderDsl
+
+  @StackBuilderDsl
+  inner class StackBuilder(
+    private val add: (key: KeyType?, primitive: Primitive) -> Unit,
+  ) {
+
+    fun rectangle(tag: KeyType? = null, x: Int = 0, y: Int = 0, width: Int = 100,
+      height: Int = 100) {
+      add(tag, Rectangle(x, y, width, height))
     }
+  }
+
+  fun EmptySpaceClicked<*>.createStackHereFor(tag: KeyType, buildStack: StackBuilder.() -> Unit) {
+    Stack(x, y, 0, 0).also {
+      root.add(it, tag)
+      StackBuilder { key, primitive -> it.add(primitive, key) }.buildStack()
+      it.recalculateSize()
+    }
+
     redraw()
   }
 
@@ -94,26 +154,26 @@ class GraphicalLayer<Key : Hashable>(
         save()
         fillRect(0.0, 0.0, canvas.width.toDouble(), canvas.height.toDouble())
         translate(0.5, 0.5)
-        draw(primitives)
+        draw(root)
         restore()
       }
     }
   }
 
-  fun CanvasRenderingContext2D.draw(primitives: List<Primitive>) {
+  private fun CanvasRenderingContext2D.draw(primitives: List<Primitive>) {
     for (primitive in primitives) {
       draw(primitive)
     }
   }
 
-  fun CanvasRenderingContext2D.draw(primitive: Primitive) {
+  private fun CanvasRenderingContext2D.draw(primitive: Primitive) {
     when (primitive) {
       is Rectangle -> draw(primitive)
       is Stack -> draw(primitive)
     }
   }
 
-  fun CanvasRenderingContext2D.draw(rectangle: Rectangle) {
+  private fun CanvasRenderingContext2D.draw(rectangle: Rectangle) {
     strokeRect(
         rectangle.x.toDouble(),
         rectangle.y.toDouble(),
@@ -122,45 +182,50 @@ class GraphicalLayer<Key : Hashable>(
     )
   }
 
-  fun CanvasRenderingContext2D.draw(stack: Stack) {
-    save()
-    translate(stack.x.toDouble(), stack.y.toDouble())
-    stack.primitives.forEach { draw(it) }
-    restore()
+  private fun CanvasRenderingContext2D.draw(stack: Stack) {
+    hierarchy[stack]?.also {
+      save()
+      translate(stack.x.toDouble(), stack.y.toDouble())
+      draw(it)
+      restore()
+    }
+  }
+
+  companion object {
+
+    private var Primitive.x: Int
+      get() = when (this) {
+        is Rectangle -> x
+        is Stack -> x
+      }
+      set(value) = when (this) {
+        is Rectangle -> x = value
+        is Stack -> x = value
+      }
+
+    private var Primitive.y: Int
+      get() = when (this) {
+        is Rectangle -> y
+        is Stack -> y
+      }
+      set(value) = when (this) {
+        is Rectangle -> y = value
+        is Stack -> y = value
+      }
+
+    private val Primitive.width: Int
+      get() = when (this) {
+        is Rectangle -> width
+        is Stack -> width
+      }
+
+    private val Primitive.height: Int
+      get() = when (this) {
+        is Rectangle -> height
+        is Stack -> height
+      }
   }
 }
-
-var GraphicalLayer.Primitive.x: Int
-  get() = when (this) {
-    is GraphicalLayer.Rectangle -> x
-    is GraphicalLayer.Stack -> x
-  }
-  set(value) = when (this) {
-    is GraphicalLayer.Rectangle -> x = value
-    is GraphicalLayer.Stack -> x = value
-  }
-
-private var GraphicalLayer.Primitive.y: Int
-  get() = when (this) {
-    is GraphicalLayer.Rectangle -> y
-    is GraphicalLayer.Stack -> y
-  }
-  set(value) = when (this) {
-    is GraphicalLayer.Rectangle -> y = value
-    is GraphicalLayer.Stack -> y = value
-  }
-
-private val GraphicalLayer.Primitive.width: Int
-  get() = when (this) {
-    is GraphicalLayer.Rectangle -> width
-    is GraphicalLayer.Stack -> width
-  }
-
-private val GraphicalLayer.Primitive.height: Int
-  get() = when (this) {
-    is GraphicalLayer.Rectangle -> height
-    is GraphicalLayer.Stack -> height
-  }
 
 data class StringTag(val value: String) : Hashable
 
@@ -174,20 +239,21 @@ fun newInit() {
   canvas.width = window.innerWidth
   canvas.height = window.innerHeight
 
+  val randomName = { generateSequence("tag"::random).take(5).joinToString("") }
   val graphicalLayer: GraphicalLayer<StringTag> = GraphicalLayer(
       canvas = canvas,
       onClick = { e ->
         when (e) {
-          is GraphicalLayer.EmptySpaceClicked -> e.createStackHereFor(
-              StringTag("tag"),
-              GraphicalLayer.Rectangle(height = 30, width = 200),
-              GraphicalLayer.Rectangle(y = 30, height = 100, width = 30),
-              GraphicalLayer.Rectangle(x = 170, y = 30, height = 100, width = 30),
-              GraphicalLayer.Rectangle(x = 30, y = 30, height = 100, width = 140),
-          )
+          is GraphicalLayer.EmptySpaceClicked -> e.createStackHereFor(StringTag(randomName())) {
+            rectangle(StringTag("name"), height = 30, width = 200)
+            rectangle(StringTag("input"), y = 30, height = 100, width = 30)
+            rectangle(StringTag("output"), x = 170, y = 30, height = 100, width = 30)
+            rectangle(StringTag("body"), x = 30, y = 30, height = 100, width = 140)
+          }
 
           is GraphicalLayer.PrimitiveClicked -> {
-            console.log(e)
+            // todo add behaviour like "click-to-connect" or "click-to-rename"
+            console.log(e.path.joinToString(" > "))
           }
         }
       },
